3195b39b8b57e5175976f39a8cd1b993228a3ddb
CleoQc <cleoqc1124@gmail.com>
2016-10-14 14:48:20 -0400

    code cleanup. Use new name for easygopigo library. Use from <library> import *

--- m05_stopForObstacle.py
+++ m05_stopForObstacle.py
@@ -1,29 +1,30 @@
-import easygopigo
-import gopigo
-import time
+from easygopigo import *
+from gopigo import *
+from time import sleep
 import atexit
 
 @atexit.register
 # this will be called if the program is killed via 
 # a Ctrl-C on the keyboard or any other reason
 def cleanup():
-	gopigo.stop()
+	stop()
 	
 	
 # Let's start the GoPiGo. 
 # this command only needs to be given once 
 # GoPiGo will keep going forward until told to stop
 print("Going forward")
-gopigo.fwd()
+forward()
 
 # let's name our ultrasonic sensor
-my_ultrasonic = easygopigo.UltraSonicSensor()
+my_ultrasonic = UltraSonicSensor("A1")
 
 # Note the absence of Wait code, or time.sleep in Python
 # As GoPiGo is not using broadcast events in Python
 # there's no need to space the queries out
 # It's possible to loop as quickly as possible
 # and get precise behavior.
+print("To stop the forever loop, use Ctrl-C on the keyboard")
 while True:
 	dist = my_ultrasonic.read()
 
@@ -31,6 +32,6 @@ while True:
 	# there's a danger here if we simply check for less than 40 cm
 	if dist > 0 and dist < 40:
 		print("Object too close. Stopping!")
-		gopigo.stop()
+		stop()
 		break  # this break forces the while loop to quit
 
